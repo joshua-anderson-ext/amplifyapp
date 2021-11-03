b2840aa1097ed0666dbd38e2e01a7bfd55ff188f
joshua-anderson-ext <79172763+joshua-anderson-ext@users.noreply.github.com>
2021-11-03 10:28:56 -0400

    Revert "updated public and src folders"
    This reverts commit 0b8816a2b302503caccfb37633f3a42ac8860152.

--- src/App.js
+++ src/App.js
@@ -2,25 +2,17 @@ import './styles/style.css';
 import Header from './Header.js';
 import Footer from './Footer.js';
 import Dashboard from './components/dashboard.js';
-//import Amplify, { Auth } from 'aws-amplify';
-//import awsconfig from './aws-exports';
-//import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 
-//Amplify.configure(awsconfig);
-
-//https://docs.amplify.aws/lib/auth/getting-started/q/platform/js/#option-1-use-pre-built-ui-components
-//<AmplifySignOut />
 function App() {
   return (
     <div className="App h-100">      
-      <Header/>
-      <main className="d-flex flex-column justify-content-between">
-        <Dashboard/>
-        <Footer/>
-      </main>
-    </div>
+    <Header/>
+    <main className="d-flex flex-column justify-content-between">
+      <Dashboard/>
+      <Footer/>
+    </main>
+  </div>
   );
 }
 
-//export default withAuthenticator(App);
 export default App;
